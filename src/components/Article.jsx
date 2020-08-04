0a3d5e6ff2e9a59dc4aa138decf39455e64fdb76
Sergio Sanz <sergios98@hotmail.com>
2020-08-04 12:48:49 +0200

    Componente NextPrev extraído de Article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Link } from 'gatsby';
 import Img from 'gatsby-image';
 
+import ArticleNextPrev from './ArticleNextPrev';
 import Details from './Details';
 import TOC from './TOC';
 import useOutsideClick from '../hooks/useOutsideClick';
@@ -49,32 +49,11 @@ const Article = ({ frontmatter, html, timeToRead, pageContext }) => {
           <TOC headings={ headings } />
         </aside>
       </div>
-      <div className="article__footer">
-        <div className="article__more">
-          { prev &&
-            <Link to={ prev.fields.slug } className="more__prev">
-              <div className="more__wrapper">
-              <i className="icon icon--arrow-left"></i>
-                <div className="more__text">
-                  <span>Anterior</span>
-                  <h2>{ prev.frontmatter.title }</h2>
-                </div>
-              </div>
-            </Link>
-          }
-          { next &&
-            <Link to={ next.fields.slug } className="more__next">
-              <div className="more__wrapper">
-                <div className="more__text">
-                  <span>Siguiente</span>
-                  <h2>{ next.frontmatter.title }</h2>
-                </div>
-                <i className="icon icon--arrow-right"></i>
-              </div>
-            </Link>
-          }
+      { (next || prev) &&
+        <div className="article__footer">
+          <ArticleNextPrev next={ next } prev={ prev } />
         </div>
-      </div>
+      }
       { headings.titles.length > 0 && <button
         className={ 'toc-toggle' + (TOCActive ? ' toc-toggle--active' : '') }
         role="switch"
